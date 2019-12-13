e70737c3b1642b4e0faf76fc1dcdbf1035060859
Spotty Savanna <spottysavanna@gmail.com>
2019-12-13 19:43:20 +0200

    Done "Blueprints and Views"

--- flask-tutorial/flaskr/__init__.py
+++ flask-tutorial/flaskr/__init__.py
@@ -27,9 +27,12 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello/')
     def hello():
-        return 'Hello, World! sdjfsdfkkk'
+        return 'Hello, World!'
     
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
